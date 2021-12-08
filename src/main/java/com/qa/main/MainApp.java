41f2da33b6c900740c998da23fb3b73db5eda359
Joker Ganteng <you@example.com>
2021-12-09 00:16:40 +0700

    Belajar JUnit

---
+++ src/main/java/com/qa/main/MainApp.java
@@ -0,0 +1,10 @@
+package com.qa.main;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
---
+++ src/main/java/com/qa/main/function/Function.java
@@ -0,0 +1,13 @@
+package com.qa.main.function;
+
+public class Function {
+	
+	public double hitungKecepatan(double jarak, double waktu) {
+		
+		return jarak/waktu;
+	}
+
+
+
+
+}
